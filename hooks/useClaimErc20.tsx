d25638565a9d879f819029918eacafaa9f5819e6
Florin Georgescu <flo_georgescu@hotmail.com>
2024-05-13 18:12:14 +0300

    Added modal context to use claim hooks

--- hooks/useClaimErc20.tsx
+++ hooks/useClaimErc20.tsx
@@ -2,6 +2,7 @@ import { ethers } from "ethers";
 import { useCallback, useEffect, useState } from "react";
 import { useContract } from "./useContract";
 import { useConnection } from "./useConnection";
+import { useWalletPopup } from "../context/wallet-popup";
 
 export const useClaimErc20 = () => {
   const { account } = useConnection();
@@ -9,44 +10,45 @@ export const useClaimErc20 = () => {
     useContract();
   const [isLoading, setIsLoading] = useState(false);
   const [state, setState] = useState({ status: "", error: "" });
+  const { asyncCallback } = useWalletPopup();
 
-  const claim = useCallback(
-    async (address: string, onSuccess: () => void) => {
-      if (
-        !taraConnectorContract ||
-        !erc20MintingConnectorContract ||
-        !account ||
-        !address
-      ) {
-        setState({ status: "Fail", error: "Contract not available" });
-        return;
-      }
-
-      try {
-        setIsLoading(true);
-        const feeToClain = await taraConnectorContract.feeToClaim(address);
-        const valueInEther = ethers.utils.formatEther(feeToClain.toString());
-        const tx = await erc20MintingConnectorContract.claim(address, {
-          value: ethers.utils.parseEther(valueInEther),
-        });
-        await tx.wait();
-        setIsLoading(false);
-        setState({ status: "Claim successful", error: "" });
-        onSuccess();
-      } catch (error: any) {
-        setIsLoading(false);
-        const errorMessage = error.reason
-          ? error.reason.split(":").pop()?.trim()
-          : error.message;
-
-        setState({
-          status: "Fail",
-          error: errorMessage,
-        });
-      }
+  const onClaim = useCallback(
+    async (address: string) => {
+      const feeToClain = await taraConnectorContract!.feeToClaim(address);
+      const valueInEther = ethers.utils.formatEther(feeToClain.toString());
+      return await erc20MintingConnectorContract!.claim(address, {
+        value: ethers.utils.parseEther(valueInEther),
+      });
     },
-    [account, erc20MintingConnectorContract, taraConnectorContract]
+    [taraConnectorContract, erc20MintingConnectorContract]
   );
+
+  const claim = async (address: string, onSuccess: () => void) => {
+    if (
+      !taraConnectorContract ||
+      !erc20MintingConnectorContract ||
+      !account ||
+      !address
+    ) {
+      setState({ status: "Fail", error: "Contract not available" });
+      return;
+    }
+    setIsLoading(true);
+    asyncCallback(
+      async () => {
+        return await onClaim(address);
+      },
+      () => {
+        setIsLoading(false);
+        setState({ status: "Lock successful", error: "" });
+        onSuccess();
+      },
+      () => {
+        setIsLoading(false);
+        setState({ status: "Fail", error: "Transaction failed" });
+      }
+    );
+  };
 
   const resetState = () => {
     setState({ status: "", error: "" });
