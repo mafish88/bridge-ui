71b4e3696b7debe94b636c94b6a3ffdf1dea38ff
Leonard Mocanu <leonard.mocanu@ymail.com>
2024-06-13 12:30:22 +0300

    cleanup networks

--- types/networks.ts
+++ types/networks.ts
@@ -17,9 +17,6 @@ export interface Network {
     symbol: string;
     decimals: number;
   };
-  indexerUrl: string;
-  graphqlUrl: string;
-  faucetUrl: string;
 }
 export interface Networks {
   [key: number]: Network;
@@ -28,7 +25,7 @@ export interface Networks {
 export const networks: Networks = {
   841: {
     chainId: 841,
-    chainName: "Taraxa Mainnet",
+    chainName: "Taraxa",
     rpcUrl: "https://rpc.mainnet.taraxa.io/",
     iconUrl: "https://community.taraxa.io/logo192.png",
     blockExplorerUrl: "https://mainnet.explorer.taraxa.io/",
@@ -36,10 +33,7 @@ export const networks: Networks = {
       name: "TARA",
       symbol: "TARA",
       decimals: 18,
-    },
-    indexerUrl: "https://indexer.mainnet.explorer.taraxa.io",
-    graphqlUrl: "https://graphql.mainnet.taraxa.io/",
-    faucetUrl: "",
+    }
   },
   842: {
     chainId: 842,
@@ -51,10 +45,7 @@ export const networks: Networks = {
       name: "TARA",
       symbol: "TARA",
       decimals: 18,
-    },
-    indexerUrl: "https://indexer.testnet.explorer.taraxa.io",
-    graphqlUrl: "https://graphql.testnet.taraxa.io/",
-    faucetUrl: "https://faucet-testnet.explorer.taraxa.io/",
+    }
   },
   843: {
     chainId: 843,
@@ -66,10 +57,7 @@ export const networks: Networks = {
       name: "TARA",
       symbol: "TARA",
       decimals: 18,
-    },
-    indexerUrl: "https://indexer.devnet.explorer.taraxa.io",
-    graphqlUrl: "https://graphql.devnet.taraxa.io/",
-    faucetUrl: "https://faucet-devnet.explorer.taraxa.io/",
+    }
   },
   200: {
     chainId: 200,
@@ -81,14 +69,11 @@ export const networks: Networks = {
       name: "TARA",
       symbol: "TARA",
       decimals: 18,
-    },
-    indexerUrl: "https://indexer.devnet.explorer.taraxa.io",
-    graphqlUrl: "https://graphql.devnet.taraxa.io/",
-    faucetUrl: "https://faucet-devnet.explorer.taraxa.io/",
+    }
   },
   1: {
-    chainId: 1, // Ethereum Mainnet chainId
-    chainName: "Ethereum Mainnet",
+    chainId: 1,
+    chainName: "Ethereum",
     rpcUrl: "https://mainnet.infura.io/v3/",
     iconUrl: "/ethereum-eth-logo-diamond-purple.svg",
     blockExplorerUrl: "https://etherscan.io/",
@@ -96,56 +81,18 @@ export const networks: Networks = {
       name: "Ether",
       symbol: "ETH",
       decimals: 18,
-    },
-    indexerUrl: "https://api.thegraph.com/index-node/graphql",
-    graphqlUrl: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
-    faucetUrl: "",
+    }
   },
   17000: {
-    chainId: 17000, // Ethereum PR Net Holesky chainId
-    chainName: "Holesky",
+    chainId: 17000,
+    chainName: "Ethereum Holesky",
     rpcUrl: "https://ethereum-holesky-rpc.publicnode.com",
     iconUrl: "/ethereum-eth-logo-diamond-purple.svg",
-    blockExplorerUrl: "https://holesky.beaconcha.in",
+    blockExplorerUrl: "https://holesky.etherscan.io",
     nativeCurrency: {
       name: "Ether",
       symbol: "ETH",
       decimals: 18,
-    },
-    indexerUrl: "https://api.thegraph.com/index-node/graphql",
-    graphqlUrl: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
-    faucetUrl: "",
+    }
   },
-};
-
-export const networkNameToId: { [key in NetworkName]: number } = {
-  [NetworkName.MAINNET]: 841,
-  [NetworkName.TESTNET]: 842,
-  [NetworkName.DEVNET]: 843,
-  [NetworkName.PRNET]: 200,
-  [NetworkName.ETH]: 1,
-  [NetworkName.HOLESKY]: 17000,
-};
-
-export const getNetwork = (name: NetworkName): Network | null => {
-  const id = networkNameToId[name];
-  return networks[id] || null;
-};
-
-export const getNetworkById = (chainId: number): Network | null => {
-  return networks[chainId] || null;
-};
-
-// Not really relevant anymore
-export const getNetworkSubdomain = (network: NetworkName): string => {
-  switch (network) {
-    case NetworkName.TESTNET:
-      return "testnet";
-    case NetworkName.DEVNET:
-      return "devnet";
-    case NetworkName.MAINNET:
-      return "mainnet";
-    default:
-      return "";
-  }
 };
